a4823354f03af6b50df91687019b375a8f308903
Andrew Mcconville <amcconville@acculynx.com>
2025-05-31 16:55:28 -0500

    adds more meta tags and a service worker for PWA install

---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+// This is a basic service worker for Vite + Vue 3 PWA offline support
+const CACHE_NAME = 'amc-design-cache-v1';
+const ASSETS = [
+  '/',
+  '/andrewmcconville-design/',
+  '/andrewmcconville-design/index.html',
+  '/andrewmcconville-design/manifest.json',
+  '/andrewmcconville-design/assets/icons/favicon-32x32.png',
+  '/andrewmcconville-design/assets/about/home-social-media.png',
+  // Add more assets as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
