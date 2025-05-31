affd3daced60c93b1acff31969fbcd346a647c9d
Andrew Mcconville <amcconville@acculynx.com>
2025-05-31 09:21:40 -0500

    missed trailing slashes

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
     <div>
     <ul>
       <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/universal-design-q-tip-grip">Q-Tip Grip Universal Design</RouterLink></li>
-      <li><RouterLink to="/oven-interface">Oven Interface Appliance HMI</RouterLink></li>
-      <li><RouterLink to="/stack-overflow-user-research">Stack Overflow User Research</RouterLink></li>
-      <li><RouterLink to="/ux-engineering">AccuLynx UX Engineering</RouterLink></li>
+      <li><RouterLink to="/universal-design-q-tip-grip/">Q-Tip Grip Universal Design</RouterLink></li>
+      <li><RouterLink to="/oven-interface/">Oven Interface Appliance HMI</RouterLink></li>
+      <li><RouterLink to="/stack-overflow-user-research/">Stack Overflow User Research</RouterLink></li>
+      <li><RouterLink to="/ux-engineering/">AccuLynx UX Engineering</RouterLink></li>
     </ul>
   </div>
   <div>
